c3533d05f7bd5424ece3d2992e5bf2c0cee28c79
Gintautas Plonis <gplonis@gmail.com>
2022-04-28 21:29:13 +0300

    Add initial markup

--- app.js
+++ app.js
@@ -12,12 +12,18 @@ app.use(cors({ origin: 'http://localhost:8081' }));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+app.get('/iot', async (req, res) => {
+    res.render('iot');
+});
+
+app.get('/device', async (req, res) => {
+    res.render('device');
+});
+
 app.get('/', async (req, res) => {
     const result = await prisma.user.findMany();
 
-    res.render('index', {
-        name: result[0].username,
-    })
+    res.render('index');
 });
 
 const PORT = process.env.NODE_DOCKER_PORT || 8080;
