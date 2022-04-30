06cbbe8cfd6ae6752dd635b5147ba608be506485
Gintautas Plonis <gplonis@gmail.com>
2022-04-30 17:11:56 +0300

    Fix migrations for heroku

--- prisma/migrations/20220426185942_/migration.sql
+++ prisma/migrations/20220426185942_/migration.sql
@@ -1,7 +1,7 @@
 -- CreateTable
 CREATE TABLE `Business` (
     `id` INTEGER NOT NULL AUTO_INCREMENT,
-    `title` VARCHAR(255) NOT NULL,
+    `title` VARCHAR(191) NOT NULL,
 
     PRIMARY KEY (`id`)
 ) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
@@ -9,8 +9,8 @@ CREATE TABLE `Business` (
 -- CreateTable
 CREATE TABLE `User` (
     `id` INTEGER NOT NULL AUTO_INCREMENT,
-    `username` VARCHAR(255) NOT NULL,
-    `password` VARCHAR(255) NOT NULL,
+    `username` VARCHAR(191) NOT NULL,
+    `password` VARCHAR(191) NOT NULL,
     `businessId` INTEGER NOT NULL,
 
     PRIMARY KEY (`id`)
