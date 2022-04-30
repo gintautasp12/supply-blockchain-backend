22f59dd8e0c1b8acb1f0b780c0890e8bd940f246
Gintautas Plonis <gplonis@gmail.com>
2022-04-30 17:19:33 +0300

    Fix heroku port env var name

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use('/', router);
 
-const PORT = process.env.NODE_DOCKER_PORT || 8080;
+const PORT = process.env.PORT || 8080;
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
 });
